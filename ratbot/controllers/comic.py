b8d6009d208fb39952b6f1a0518b9f73d34af1ab
Holly Jones <smeghead.h@gmail.com>
2011-09-27 20:20:34 +0000

    Fixed #3

--- ratbot/controllers/comic.py
+++ ratbot/controllers/comic.py
@@ -2,12 +2,13 @@
 """Comic Controller"""
 
 import datetime
-from tg import expose
+from tg import expose, abort
 from tg.i18n import ugettext as _, lazy_ugettext as l_
 from repoze.what import predicates
 
 from ratbot.lib.base import BaseController
 from ratbot.model import DBSession, Comic, Issue, Page
+from sqlalchemy.orm.exc import NoResultFound
 
 __all__ = ['ComicController']
 
@@ -24,35 +25,47 @@ class ComicController(BaseController):
 
     @expose('ratbot.templates.comic')
     def view(self, comic, issue, page):
-        return dict(
-            method='comic',
-            comics=DBSession.query(Comic).order_by(Comic.title),
-            page=DBSession.query(Page).\
-                filter(Page.comic_id==comic).\
-                filter(Page.issue_number==issue).\
-                filter(Page.number==page).one(),
-        )
+        try:
+            return dict(
+                method='comic',
+                comics=DBSession.query(Comic).order_by(Comic.title),
+                page=DBSession.query(Page).\
+                    filter(Page.comic_id==comic).\
+                    filter(Page.issue_number==issue).\
+                    filter(Page.number==page).one(),
+            )
+        except NoResultFound:
+            abort(404)
 
     @expose(content_type='image/png')
     def thumb(self, comic, issue, page):
-        page=DBSession.query(Page).\
-            filter(Page.comic_id==comic).\
-            filter(Page.issue_number==issue).\
-            filter(Page.number==page).one()
-        return page.thumbnail
+        try:
+            page=DBSession.query(Page).\
+                filter(Page.comic_id==comic).\
+                filter(Page.issue_number==issue).\
+                filter(Page.number==page).one()
+            return page.thumbnail
+        except NoResultFound:
+            abort(404)
 
     @expose(content_type='image/png')
     def png(self, comic, issue, page):
-        page=DBSession.query(Page).\
-            filter(Page.comic_id==comic).\
-            filter(Page.issue_number==issue).\
-            filter(Page.number==page).one()
-        return page.bitmap
+        try:
+            page=DBSession.query(Page).\
+                filter(Page.comic_id==comic).\
+                filter(Page.issue_number==issue).\
+                filter(Page.number==page).one()
+            return page.bitmap
+        except NoResultFound:
+            abort(404)
 
     @expose(content_type='image/svg+xml')
     def svg(self, comic, issue, page):
-        page=DBSession.query(Page).\
-            filter(Page.comic_id==comic).\
-            filter(Page.issue_number==issue).\
-            filter(Page.number==page).one()
-        return page.vector
+        try:
+            page=DBSession.query(Page).\
+                filter(Page.comic_id==comic).\
+                filter(Page.issue_number==issue).\
+                filter(Page.number==page).one()
+            return page.vector
+        except NoResultFound:
+            abort(404)
