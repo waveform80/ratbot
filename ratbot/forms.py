3960945d6eb91cc84c95a20376bca04a53d0b1bb
Dave Jones <dave@waveform.org.uk>
2017-04-04 20:12:59 +0100

    Update copyrights

--- ratbot/forms.py
+++ ratbot/forms.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # vim: set et sw=4 sts=4:
 
-# Copyright 2012-2016 Dave Jones <dave@waveform.org.uk>
+# Copyright 2012-2017 Dave Jones <dave@waveform.org.uk>
 #
 # This file is part of ratbot comics.
 #
