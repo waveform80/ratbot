1219d929835921485c3aa294ca287eba8110b9d2
Dave Jones <dave@waveform.org.uk>
2017-04-04 19:00:47 +0100

    Bind cherrypy to localhost
    Use nginx to forward

--- server.py
+++ server.py
@@ -11,8 +11,8 @@ if __name__ == '__main__':
     cherrypy.tree.graft(app, "/")
     cherrypy.server.unsubscribe()
     server = cherrypy._cpserver.Server()
-    server.socket_host = "0.0.0.0"
-    server.socket_port = 8080
+    server.socket_host = "127.0.0.1"
+    server.socket_port = 5001
     server.thread_pool = 30
     server.subscribe()
     cherrypy.engine.start()
